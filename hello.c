b29a7a269ee14b35d2773e4610a439009b389ae3
Guillermo-dlti <memo2507.delatorre@gmail.com>
2025-11-25 08:55:49 -0600

    while

--- hello.c
+++ hello.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    // printf("Hello");
-    for(int i = 0; i < 50; i++){
-             printf("Hello\n");
-         }
-    // while(true){
-    //     for(int i = 0; i < 50; i++){
-    //         printf("Hello\n");
-    //     }
-    // }
+    while(true){
+        for(int i = 0; i < 50; i++){
+            printf("Hello\n");
+        }
+    }
     return 0;
 }
